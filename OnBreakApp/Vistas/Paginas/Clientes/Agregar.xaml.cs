60e0c0bdcb029a7287a2406f0fe1de749e893513
matias0033 <ma.villegas@duocuc.cl>
2023-04-16 19:01:45 -0400

    ULTIMOS CAMBIOS

--- OnBreakApp/Vistas/Paginas/Clientes/Agregar.xaml.cs
+++ OnBreakApp/Vistas/Paginas/Clientes/Agregar.xaml.cs
@@ -49,6 +49,16 @@ namespace Vistas.Paginas.Clientes
 
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
 
 
 
